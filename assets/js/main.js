27d492388656d7ed77ee7f98d8ae50484d0f8a84
Saheer-kp <saheerkp43@gmail.com>
2026-06-10 11:09:23 +0530

    new updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -414,7 +414,7 @@ expNavButtons.forEach(btn => {
 const typingTarget = document.getElementById("typing-text");
 
 if (typingTarget) {
-    const roles = ['PHP Laravel Architect.', 'Software Engineer', 'Node.js Developer.', 'Full-Stack Developer.'];
+    const roles = ['PHP Laravel Architect.', 'Software Engineer.', 'Node.js Developer.', 'Full-Stack Developer.'];
     let currentRoleIndex = 0;
     let currentText = '';
     let isDeleting = false;
